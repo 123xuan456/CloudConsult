e7364a956ac92c95baabcd1d0432904cd2308816
123xuan456 <1044072796@qq.com>
2018-12-28 22:28:50 +0800

    上标栏

--- app/src/main/java/com/mtm/cloudconsult/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/mtm/cloudconsult/mvp/ui/activity/MainActivity.java
@@ -4,23 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
-
+import com.mtm.cloudconsult.R;
 import com.mtm.cloudconsult.di.component.DaggerMainComponent;
 import com.mtm.cloudconsult.di.module.MainModule;
 import com.mtm.cloudconsult.mvp.contract.MainContract;
 import com.mtm.cloudconsult.mvp.presenter.MainPresenter;
 
-import com.mtm.cloudconsult.R;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
+
+
+    @BindView(R.id.iv_title_menu)
+    ImageView ivTitleMenu;
+    @BindView(R.id.ll_title_menu)
+    FrameLayout llTitleMenu;
+    @BindView(R.id.iv_title_one)
+    ImageView ivTitleOne;
+    @BindView(R.id.iv_title_two)
+    ImageView ivTitleTwo;
+    @BindView(R.id.iv_title_three)
+    ImageView ivTitleThree;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.vp_content)
+    ViewPager vpContent;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -53,6 +83,16 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public void showMessage(@NonNull String message) {
         checkNotNull(message);
         ArmsUtils.snackbarText(message);
@@ -68,4 +108,32 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     public void killMyself() {
         finish();
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.ll_title_menu, R.id.iv_title_one, R.id.iv_title_two, R.id.iv_title_three})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ll_title_menu:
+                // 开启菜单
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+                break;
+            case R.id.iv_title_one:
+                break;
+            case R.id.iv_title_two:
+                break;
+            case R.id.iv_title_three:
+                break;
+        }
+    }
+
 }
